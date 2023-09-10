5602a819f1be93397ceab1ecdb5a695f7e269bf2
Labid Al Nahiyan <68603298+labid-al-nahiyan@users.noreply.github.com>
2023-09-10 19:26:46 +0600

    footer set 50%

--- recipe/src/Component/Footer/Footer.jsx
+++ recipe/src/Component/Footer/Footer.jsx
@@ -1,13 +1,13 @@
+import { Container, Row } from 'react-bootstrap';
 import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className="footer">
+    <div className="footer">
            
 
-           <div className="container">
-        <div className="media-container-row align-center mbr-white">
-            <div className="row row-links">
+        <Container>
+            <Row>
                 <ul className="foot-menu"> 
                     <li className="foot-menu-item mbr-fonts-style display-7">
                         <a className="text-white" href="#" target="_blank">About us</a>
@@ -21,8 +21,8 @@ const Footer = () => {
                         <a className="text-white" href="#" target="_blank">Work</a>
                     </li>
                 </ul>
-            </div>
-            <div className="row social-row">
+            </Row>
+            <Row>
                 <div className="social-list align-right pb-2">      
                 <div className="soc-item">
                         <a href="https://twitter.com/mobirise" target="_blank">
@@ -49,15 +49,14 @@ const Footer = () => {
                             <span className="socicon-behance socicon mbr-iconfont mbr-iconfont-social"></span>
                         </a>
                     </div></div>
-            </div>
-            <div className="row row-copirayt">
+            </Row>
+            <Row>
                 <p className="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
                     © Copyright 2023 Team Khabo. All Rights Reserved.
                 </p>
-            </div>
-        </div>
+            </Row>
+        </Container>
     </div>
-        </div>
     );
 };
 
