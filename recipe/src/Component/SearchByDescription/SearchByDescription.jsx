acc4b3effd52030e5f6768a90e59b4607b35ae55
Sk Saif <sksaifullahhafiz@gmail.com>
2023-09-11 18:47:22 +0600

    image problem fixed partially

--- recipe/src/Component/SearchByDescription/SearchByDescription.jsx
+++ recipe/src/Component/SearchByDescription/SearchByDescription.jsx
@@ -67,7 +67,7 @@ const SearchByDescription = () => {
             </div>
             <div className="col-lg-7 offset-lg-1 col-12">
                 <div className="image-wrapper">
-                    <img className="w-100" src="assets/images/mbr-3.jpg" alt="Mobirise Website Builder" />
+                    <img className="w-100" src="../../assets/images/mbr-3.jpg" alt="Mobirise Website Builder" />
                 </div>
             </div>
         </div>
